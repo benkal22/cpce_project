6d0123ead82d1a02327df2a1b836560f3b53e671
benkal22 <benkalsoft@gmail.com>
2024-05-03 01:16:09 +0400

    Remplissage table product par la nommenclature

--- economic_exchanges/views.py
+++ economic_exchanges/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
+
+def product_home(request):
+    products = Product.objects.all()
+    return render(request, 'economic_exchanges/product.html', {'products': products})
 
 def producer_home(request):
-    return HttpResponse('<h1>Mon espace de production</h1>')
+    producers = Producer.objects.all()
+    return render(request, 'economic_exchanges/producer.html', {'producers': producers})
+
 
 def supplier_home(request):
     return HttpResponse('<h1>Mes fournisseurs</h1>')
